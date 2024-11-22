398cac37156bb1b9f72d3cccaaf476a49399d4ba
Juan Marmolejo <jmarmol1@my.centennialcollege.ca>
2024-11-22 16:01:39 -0500

    Add responsive design

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -14,15 +14,15 @@ export default async function Page() {
     const data: OverviewStocks = await dataStocksOverview.stocksData;
 
     return (
-        <div className="bg-quanternary min-h-screen">
+        <div className="min-h-screen bg-quanternary">
             <div className="relative h-[35rem] w-full overflow-hidden">
                 <Image src={stock2} alt="stock image" fill priority className="object-cover" />
             </div>
-            <div className="mx-[15%] mt-10 max-h-[70%]">
-                <h1 className="text-3xl font-bold">
+            <div className="mx-[10%] mt-10 max-w-[80%] md:mx-[15%] md:max-w-[70%]">
+                <h1 className="text-6xl font-bold">
                     Welcome to <span className="text-primary">Stockify</span>
                 </h1>
-                <h2 className="mt-2 text-lg">
+                <h2 className="mt-2 text-xl">
                     Track, analyze, and stay ahead in the stock market with our comprehensive stock
                     management platform. Whether you're monitoring your investments or exploring new
                     market trends, our tools offer the insights you need to make informed decisions.
