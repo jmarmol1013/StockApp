7b4716be298c31468f282f3f9101e2cce2fe3210
Juan Marmolejo <jmarmol1@my.centennialcollege.ca>
2024-11-25 17:12:55 -0500

    Change MetaData

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,10 @@
 import { Footer } from '@/components/Footer';
 import NavBar from '@/components/NavBar';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Stock App',
+};
 
 export default function RootLayout({
     children,
